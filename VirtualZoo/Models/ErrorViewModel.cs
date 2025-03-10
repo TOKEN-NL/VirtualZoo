4b4621e24a9e74fba54755bf072165a00b9065b8
TOKEN-NL <bergje@hotmail.nl>
2025-03-10 14:15:25 +0100

    Initial project setup with ASP.NET Core MVC

---
+++ VirtualZoo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VirtualZoo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
